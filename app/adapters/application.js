d784473b58d490fc703f7fc0c004efcf5f66a730
Jodi Detch <jodidetch@gmail.com>
2014-10-01 14:53:08 -0400

    App has an updated router; will direct user to contact(s) pages

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.ActiveModelAdapter.extend({
+export default DS.ActiveModelAdapter.extend({  // Can serialize/deserialize JSON correctly with DS.ActiveModelAdapter
   namespace: 'api/v1',
   host: 'http://localhost:3000'
 });
--- app/router.js
+++ app/router.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType // FrontEndENV instead of config?
 });
 
 Router.map(function() {
+  this.resource('contacts', function() {
+    this.resource('contact', { path: '/:contact_id' });
+  });
 });
 
 export default Router;
